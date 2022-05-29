405f13f179138c1f6a27841ee21abbe9cef9b818
Allan Araya <allanreyes0107@gmail.com>
2022-05-28 23:16:40 -0600

    Final touches

--- script.js
+++ script.js
@@ -2,9 +2,18 @@
 
 let basicSkillsTitle = document.getElementById('basic-skills-title');
 let generalSkills = document.getElementById('general-skills');
+
 let dataBaseTitle = document.getElementById('database-title');
 let databaseEnginesContent = document.getElementById('database-engines');
 
+let toolsTitle = document.getElementById('tools-title');
+let toolsContent = document.getElementById('tools-skills');
+
+let opTitle = document.getElementById('op-title');
+let opContent =document.getElementById('op-skills-cont');
+
+opContent.style.display = 'none';
+toolsContent.style.display = 'none';
 databaseEnginesContent.style.display = 'none';
 generalSkills.style.display = 'none'; 
 
@@ -17,14 +26,29 @@ basicSkillsTitle.addEventListener('click', function() {
     }
 });
 
-dataBaseTitle.addEventListener('click', function (){
+dataBaseTitle.addEventListener('click', function() {
     if (databaseEnginesContent.style.display === 'none'){
         databaseEnginesContent.style.display = 'flex';
     } else {
         databaseEnginesContent.style.display = 'none';
     }
 });
+
+toolsTitle.addEventListener('click', function() {
+    if (toolsContent.style.display === 'none'){
+        toolsContent.style.display = 'flex';
+    } else {
+        toolsContent.style.display = 'none';
+    }
+});
     
+opTitle.addEventListener('click', function() {
+    if (opContent.style.display === 'none') {
+        opContent.style.display = 'flex';
+    } else {
+        opContent.style.display = 'none';
+    }
+})
 
 
 
